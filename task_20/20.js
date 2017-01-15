4369fa5ae46f0fe8cd67c64cce5c0a9d2201a450
shendapeng <819465103@qq.com>
2017-01-15 16:33:59 +0800

    task_20 task_20 task_20

---
+++ task_20/20.js
@@ -0,0 +1,48 @@
+﻿
+
+var insert = document.getElementById('insert');
+var textarea = document.getElementById('textarea');
+var qurey = document.getElementById('qurey');
+var text = document.getElementById('text');
+var div = document.getElementById('dispaly');
+
+init();
+
+function init (argument) {
+    var s;
+     insert.onclick = function  (argument) {
+          var str = textarea.value;
+          s =str.split(/[,.，\n、" "]/);
+          for (var i = 0; i < s.length; i++) {
+                 var divchild = document.createElement('div');
+                 divchild.innerHTML=s[i];
+                 div.appendChild(divchild);
+
+          };
+
+     }
+     qurey.onclick = function (argument) {
+         for (var i = 0; i < s.length; i++) {
+              if (s[i].indexOf(text.value)!=-1)
+              {
+                var divchilds = div.children;
+                console.log(divchilds[i]);
+                if (div.children) 
+                {   
+                    var HTML ="";
+                    var strarr = divchilds[i].innerHTML.split(text.value);
+                    for (var j = 0; j < strarr.length; j++) {
+                        HTML+=strarr[j];
+                        if (j!=strarr.length-1) 
+                          {
+                              HTML+='<span>'+text.value+'</span>';
+                          }
+                      
+                    }
+                    divchilds[i].innerHTML=HTML;
+                   
+                }
+              }
+         }
+     }
+}
